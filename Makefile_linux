b4b5262e43a278b66f7d5cb81ea3d0abe91d9d08
Christopher Winkelmann <winkelchri@gmail.com>
2022-08-07 14:16:23 +0200

    Remove Godirwalk, separate build files.

---
+++ Makefile_linux
@@ -0,0 +1,5 @@
+windows:
+	env GOOS="windows" GOARCH="amd64" go build -o bin/win/cache-this.exe
+
+linux:
+	env GOOS="linux" GOARCH="amd64" go build -o bin/linux/cache-this
--- read_dir_test.go
+++ read_dir_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -11,9 +12,21 @@ var testDirectories = []string{
 	"testdir/folder space",
 }
 
+var testFiles = map[string]string{
+	"empty_file":              "",
+	"file_with_extension.txt": "Abababa",
+	"file_with_rune":          `1000011110101000010011000111101011011011100001100011010110011111000011001100100100010011101000111001110100010101100111000001011100000010111111011010100011111000`,
+}
+
 func setupTestDirectories() {
 	for _, d := range testDirectories {
 		os.MkdirAll(d, os.ModePerm)
+		for fname, fdata := range testFiles {
+			dst := filepath.Join(d, fname)
+			if err := os.WriteFile(dst, []byte(fdata), 0666); err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 }
 
@@ -28,7 +41,10 @@ func deleteTestDirectories() {
 }
 
 func TestGetDirectoryInfo(t *testing.T) {
+
 	for _, testDir := range testDirectories {
+		testDir = filepath.Clean(testDir)
+
 		t.Run(testDir, func(t *testing.T) {
 			cacheDir, err := GetDirectoryInfo(testDir)
 			if err != nil {
